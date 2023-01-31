68146af3009f205d9a1cf6c4b1d266aa99e0ce05
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-01-31 22:37:25 +0000

    Corrected current date format in <p> in header to reflect mockUp

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const today = moment().format("dddd, D MMMM YYYY");
+// Display today's date in the format Tuesday, January 31 as per the mockup
+const today = moment().format("dddd, MMMM D ");
 const currentDay = $("#currentDay");
 currentDay.text(today);
+console.log(currentDay);
